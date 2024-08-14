533c08c46d89cd7a0393f51cee2cc21832ba15c0
ryu <114303361+ryuapp@users.noreply.github.com>
2024-08-14 16:14:03 +0900

    refactor: divide browser and server files

--- src/md-to-pdf.ts
+++ src/md-to-pdf.ts
@@ -1,25 +1,5 @@
-import { Hono } from "@hono/hono/tiny";
-import { LinearRouter } from "@hono/hono/router/linear-router";
 import { launch } from "@astral/astral";
-import { init as initMd4w, mdToHtml } from "md4w";
-
-const DEFAULT_PORT = 33433;
-
-await initMd4w("fast");
-/**
- * Launch a HTTP server for serving converted markdown to HTML.
- *
- * @param path a markdown file path
- */
-function launchHttpServer(path: string): Deno.HttpServer<Deno.NetAddr> {
-  const app = new Hono({ router: new LinearRouter() });
-  const decoder = new TextDecoder("utf-8");
-  app.get("/", async (c) => {
-    const content = mdToHtml(decoder.decode(await Deno.readFile(path)));
-    return c.html(content);
-  });
-  return Deno.serve({ onListen: () => "", port: DEFAULT_PORT }, app.fetch);
-}
+import { DEFAULT_PORT, launchHttpServer } from "./server.ts";
 
 /**
  * Convert a markdown file to PDF.
---
+++ src/server.ts
@@ -0,0 +1,35 @@
+import { Hono } from "@hono/hono/tiny";
+import { LinearRouter } from "@hono/hono/router/linear-router";
+import { init as initMd4w, mdToHtml } from "md4w";
+
+export const DEFAULT_PORT = 33433;
+
+await initMd4w("small");
+
+/**
+ * Launch a HTTP server for serving converted markdown to HTML.
+ *
+ * @param path a markdown file path
+ */
+export function launchHttpServer(
+  path: string,
+): Deno.HttpServer<Deno.NetAddr> {
+  const app = new Hono({ router: new LinearRouter() });
+  const decoder = new TextDecoder("utf-8");
+
+  app.get("/", async (c) => {
+    const content = mdToHtml(
+      decoder.decode(await Deno.readFile(path)),
+    );
+    return c.html(
+      `<html>
+          <head>
+          </head>
+          <body>
+            ${content}
+          </body>
+        </html>`,
+    );
+  });
+  return Deno.serve({ onListen: () => "", port: DEFAULT_PORT }, app.fetch);
+}
