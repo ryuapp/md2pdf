81807f79ef1075c9d2d8fae6a82cc844ba44a4fc
ryu <114303361+ryuapp@users.noreply.github.com>
2024-12-08 02:31:34 +0900

    test: use documentation tests

--- src/utils/filename.ts
+++ src/utils/filename.ts
@@ -1,7 +1,16 @@
 /**
  * Get a filename without extname from path.
  *
- * @param path
+ * @example
+ * ```ts
+ * import { assertEquals } from "@std/assert";
+ *
+ * assertEquals(getFilename("README.md"), "README");
+ * assertEquals(getFilename("/README.md"), "/README");
+ * assertEquals(getFilename("./README.md"), "./README");
+ * assertEquals(getFilename("../README.md"), "../README");
+ * assertEquals(getFilename("README.2.md"), "README.2");
+ * ```
  */
 export function getFilename(path: string): string {
   return path.split(".").slice(0, -1).join(".");
